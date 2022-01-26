5b9ef987676d2bb82a8e63cd9d985905a911de15
Hafijul Islam <hafijul233@gmail.com>
2022-01-26 14:43:42 +0600

    config module and service created

---
+++ app/config/app/config.module.ts
@@ -0,0 +1,29 @@
+import * as Joi from '@hapi/joi';
+import { Module } from '@nestjs/common';
+import configuration from './configuration';
+import { AppConfigService } from './config.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+/**
+ * Import and provide app configuration related classes.
+ *
+ * @module
+ */
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      validationSchema: Joi.object({
+        APP_NAME: Joi.string().default('MyApp'),
+        APP_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        APP_URL: Joi.string().default('http://my-app.test'),
+        APP_PORT: Joi.number().default(9000),
+      }),
+    }),
+  ],
+  providers: [ConfigService, AppConfigService],
+  exports: [ConfigService, AppConfigService],
+})
+export class AppConfigModule {}
