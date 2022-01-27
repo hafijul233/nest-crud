e32ba841a8b6af847143e80813cd51337b8f419b
Hafijul Islam <hafijul233@gmail.com>
2022-01-27 20:20:30 +0600

    php removed

--- app/config/site.config.ts
+++ app/config/site.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('app', () => ({
+export default registerAs('site', () => ({
   /*
   |--------------------------------------------------------------------------
   | Application Name
